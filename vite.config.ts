d995cf22694d9ca8b3944941f15f1c11e3178aa0
1vanguard <1vanguard@mail.ru>
2023-12-21 14:30:19 +0300

    Vite config update for remote server

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,11 @@ export default defineConfig({
     },
     server: {
         host: true,
+        port: 5174,
+        https: {
+            key: '/home/citonex/conf/web/dev.adminalgo.ae/ssl/dev.adminalgo.ae.key',
+            cert: '/home/citonex/conf/web/dev.adminalgo.ae/ssl/dev.adminalgo.ae.pem',
+        }
     },
     base: './',
 });
